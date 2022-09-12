47b6f34221fed87593a02e996e3c43b2e4265184
Decodedal <Palumbodallas@gmail.com>
2022-09-12 18:57:57 -0400

    context and ref

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { useEffect, useState } from 'react'
-import Gallery from './components/Gallery'
-import SearchBar from './components/SearchBar'
-import {DataContext} from './context/DataContext'
+import { useEffect, useState, useRef } from 'react';
+import Gallery from './components/Gallery';
+import SearchBar from './components/SearchBar';
+import {DataContext} from './context/DataContext';
+import {SearchContext} from './context/SearchContext';
 
 function App(){
     let [search, setSearch] = useState('')
     let [message, setMessage] = useState('Search for Music!')
     let [data, setData] = useState([])
+    let searchInput = useRef('')
 
     const API_URL = "https://itunes.apple.com/search?term="
 
@@ -34,7 +36,12 @@ function App(){
 
     return (
         <div>
-            <SearchBar handleSearch ={handleSearch}/>
+          <SearchContext.Provider value={{
+            term: searchInput,
+            handleSearch: handleSearch
+          }}>
+            <SearchBar handleSearch/>
+            </SearchContext.Provider>
             {message}
             <DataContext.Provider value ={data}>
             <Gallery/>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,15 @@
                 
-        import { useState } from 'react'
-        function SearchBar(props) {
+        import { useContext} from 'react'
+        import{SearchContext} from '../context/SearchContext'
+
+        function SearchBar() {
         
+            const {term, handleSearch} = useContext(SearchContext)
 
             return (
                 <form>
-                    <input type="text" placeholder="Enter a search term here"
-                    onChange={(e)=>props.handleSearch(e,e.target.value)} />
+                    <input ref={term} type="text" placeholder="Enter a search term here"
+                    onChange={(e)=>props.handleSearch(e, term)} />
                     <input type="submit" />
                     <input type="submit" value= "clear" onSubmit={(e)=>
                         props.handleSearch(e,'')
