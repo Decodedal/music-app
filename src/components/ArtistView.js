6eea978d691980f524fba7fd19c9de83290fe06a
Decodedal <Palumbodallas@gmail.com>
2022-09-15 22:30:22 -0400

    Try try again

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,17 +1,27 @@
-// These components will be making separate API calls from the app
-// component to serve specific data about our artist
 import { useState, useEffect } from 'react'
-import React from 'react'
-import {useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom'
 
 function ArtistView() {
-    const {id} =useParams()
+    const { id } = useParams()
     const [ artistData, setArtistData ] = useState([])
+
+    useEffect(() => {
+        const API_URL = `http://localhost:3000/album/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setArtistData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+
 
     return (
         <div>
             <h2>The id passed was: {id}</h2>
             <p>Artist Data Goes Here!</p>
+            {artistData}
         </div>
     )
 }
